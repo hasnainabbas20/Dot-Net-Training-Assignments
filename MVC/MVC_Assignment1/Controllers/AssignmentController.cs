061772a56d40f229197e11c8c550a3f06accf8fb
Hasnain Abbas Tinwala <hasnainabbastw786@gmail.com>
2023-02-11 17:30:24 +0530

    MVC Assignment 1

---
+++ MVC/MVC_Assignment1/Controllers/AssignmentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Assignment1.Models;
+using System.Data.Entity;
+
+namespace MVC_Assignment1.Controllers
+{   
+    public class AssignmentController : Controller
+    {
+        NorthwindEntities1 db = new NorthwindEntities1();
+        // GET: Assignment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CustInGermany()
+        {
+            return View(db.Cust_Germany().ToList());
+        }
+
+        public ActionResult CustDetails()
+        {
+            return View(db.Orders.Where(a => a.OrderID == 10248).ToList());
+        }
+    }
+}
